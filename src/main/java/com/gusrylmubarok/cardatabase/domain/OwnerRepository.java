345e78d44b967524432e3a23d18190add6e7cf63
Agus S. Mubarok <gusrylmubarok@gmail.com>
2022-11-16 11:47:22 +0700

    Create Web Service for car

--- src/main/java/com/gusrylmubarok/cardatabase/domain/OwnerRepository.java
+++ src/main/java/com/gusrylmubarok/cardatabase/domain/OwnerRepository.java
@@ -1,2 +1,6 @@
-package com.gusrylmubarok.cardatabase.domain;public interface OwnerRepository {
+package com.gusrylmubarok.cardatabase.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
 }
---
+++ src/main/java/com/gusrylmubarok/cardatabase/web/CarController.java
@@ -0,0 +1,18 @@
+package com.gusrylmubarok.cardatabase.web;
+
+import com.gusrylmubarok.cardatabase.domain.Car;
+import com.gusrylmubarok.cardatabase.domain.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CarController {
+    @Autowired
+    private CarRepository repository;
+
+    @RequestMapping("/cars")
+    public Iterable<Car> getCars() {
+        return repository.findAll();
+    }
+}
