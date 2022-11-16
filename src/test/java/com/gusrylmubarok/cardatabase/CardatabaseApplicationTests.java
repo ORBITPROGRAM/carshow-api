eaece6943c2cba441c3793d0e968dc2cb2404c61
Agus S. Mubarok <gusrylmubarok@gmail.com>
2022-11-16 19:49:01 +0700

    Make secure with JWT and testing

--- src/test/java/com/gusrylmubarok/cardatabase/CardatabaseApplicationTests.java
+++ src/test/java/com/gusrylmubarok/cardatabase/CardatabaseApplicationTests.java
@@ -1,13 +1,19 @@
 package com.gusrylmubarok.cardatabase;
 
+import com.gusrylmubarok.cardatabase.web.CarController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CardatabaseApplicationTests {
+	@Autowired
+	private CarController carController;
 
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(carController);
 	}
 
 }
